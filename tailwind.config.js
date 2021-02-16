2a90991aebed06dde06c23bad42a888a66b98c0e
unknown <pooldeveloper08@gmail.com>
2021-02-16 12:30:51 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      bre: ['Bree Serif'],
+      dancing: ['Dancing Script']
+    },
+    extend: {
+      colors: {
+        pizza: '#ff5733',
+        grio:'#4c4c4c',
+        grisc:'#7e7e7e',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
